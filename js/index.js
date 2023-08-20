89a4b6368a5c1a8eb90262e32d019025769b49cd
Maria Achkouty <61360470+mariaachkouty@users.noreply.github.com>
2023-08-21 00:12:26 +0300

    a elem

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const navLinks =  document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click',() => {
     document.body.classList.toggle('nav-open');
+
 });
 
 navLinks.forEach(link =>{
